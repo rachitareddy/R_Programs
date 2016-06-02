1839294940a88f3e977dcb9693b644d18dcf11a9
rachitareddy <rreddy2@barclaycardus.com>
2016-06-02 18:48:01 -0400

    R programs

---
+++ cacheSolve.R
@@ -0,0 +1,13 @@
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        if (!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setInverse(inv)
+        inv
+}
